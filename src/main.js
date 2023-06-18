4d8d824f6ad4eb311271d2e7ccd5e06bff0e3c31
rishik0706 <rishikaran0706@gmail.com>
2023-06-19 05:27:52 +0530

    backend post

--- src/main.js
+++ src/main.js
@@ -9,84 +9,8 @@ app.use(cors()); // allowing everyone.
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-async function addrecord(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
 
-  const db = client.db("mydb");
-  const messageColl = db.collection("message");
 
-  let inputDoc = {
-    message: req.query.message || "default",
-  };
-  await messageColl.insertOne(inputDoc);
-
-  await client.close();
-
-  // string response
-  // res.send("record added")
-
-  // json response :: preferred
-  res.json({ opr: "success" });
-}
-
-async function findAllMessage(req, res) {
-  const uri = "mongodb://127.0.0.1:27017";
-  const client = new MongoClient(uri);
-
-  const db = client.db("mydb");
-  const messageColl = db.collection("message");
-
-  let list = await messageColl.find().toArray();
-
-  await client.close();
-  res.json(list);
-}
-
-function helloPost(req, res) {
-  let result = { opr: true };
-  res.json(result);
-}
-
-// NEW TODO API
-async function addTodo(req, res) {
-  try {
-    const uri = "mongodb://127.0.0.1:27017";
-    const client = new MongoClient(uri);
-
-    const db = client.db("project");
-    const messageColl = db.collection("todo");
-
-    let inputDoc = {
-      task: req.query.task,
-      description: req.query.description,
-      numpref: req.query.numpref,
-    };
-    await messageColl.insertOne(inputDoc);
-
-    await client.close();
-    res.json({ opr: true });
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-}
-
-async function findAllTodo(req, res) {
-  try {
-    const uri = "mongodb://127.0.0.1:27017";
-    const client = new MongoClient(uri);
-
-    const db = client.db("project");
-    const messageColl = db.collection("todo");
-
-    let list = await messageColl.find().toArray();
-
-    await client.close();
-    res.json(list);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-}
 
 async function addUserRecord(req, res) {
   try {
@@ -156,31 +80,31 @@ async function deleteUserRecord(req, res) {
 }
 
 // LOGIN - AUTHENTICATION
-async function loginByGet(req, res) {
-  try {
-    const uri = "mongodb://127.0.0.1:27017";
-    const client = new MongoClient(uri);
+// async function loginByGet(req, res) {
+//   try {
+//     const uri = "mongodb://127.0.0.1:27017";
+//     const client = new MongoClient(uri);
 
-    const db = client.db("project");
-    const messageColl = db.collection("user");
+//     const db = client.db("project");
+//     const messageColl = db.collection("user");
 
-    let query = { email: req.query.email, password: req.query.password };
-    let userRef = await messageColl.findOne(query);
+//     let query = { email: req.query.email, password: req.query.password };
+//     let userRef = await messageColl.findOne(query);
 
-    await client.close();
+//     await client.close();
 
-    // Negative: UserRef CANBE NULL;
-    if (!userRef) {
-      let errorMessage = `Record Not Found or Authentication Failure: ${req.query.email}`;
-      throw new Error(errorMessage);
-    }
+//     // Negative: UserRef CANBE NULL;
+//     if (!userRef) {
+//       let errorMessage = `Record Not Found or Authentication Failure: ${req.query.email}`;
+//       throw new Error(errorMessage);
+//     }
 
-    // Postive Scenario
-    res.json(userRef);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-}
+//     // Postive Scenario
+//     res.json(userRef);
+//   } catch (err) {
+//     res.status(500).send(err.message);
+//   }
+// }
 
 async function loginByPost(req, res) {
   try {
@@ -209,16 +133,12 @@ async function loginByPost(req, res) {
 }
 
 // http://localhost:4000/addrecord
-app.get("/addrecord", addrecord);
-app.get("/findAll", findAllMessage);
-app.post("/hello", helloPost);
-app.get("/addtodo", addTodo);
-app.get("/find-all-todo", findAllTodo);
+
 app.get("/adduser", addUserRecord);
 app.get("/delete-user", deleteUserRecord);
 app.get("/find-all-user", findAllUser);
 
-app.get("/login-by-get", loginByGet);
+// app.get("/login-by-get", loginByGet);
 app.post("/login-by-post", loginByPost);
 
 // http://localhost:4000/
